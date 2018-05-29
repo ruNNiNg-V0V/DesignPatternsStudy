868329fc035121128b6c1de30e9dcc8c4df69fd9
V0V <grapduck@gmail.com>
2018-05-29 15:12:34 +0900

    상속이 아니라 인터페이스로 만들어야 함

---
+++ DesignPatternsStudy/OffensiveOperator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DesignPatternsStudy
+{
+    public class OffensiveOperator
+    {
+        public OffensiveOperator(int Amour, int Speed)
+        {
+            Dronning = 2;
+        }
+
+        public int Dronning { get; internal set; }
+
+        public void ActivateDron()
+        {
+            Dronning -= 1;
+            Console.WriteLine("드론을 작동합니다");
+        }
+    }
+}
